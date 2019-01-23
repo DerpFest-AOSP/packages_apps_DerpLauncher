b3b54bc434d1566d855e318f1504cb1d9423faa8
Joey <joey@lineageos.org>
2019-01-23 17:49:25 +0100

    DerpLauncher: Implement hidden & protected apps
    Squashed with:
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Sat Nov 2 15:23:42 2019 +0100
    
        Switch to BiometricPrompt
    
        Change-Id: Id6700c81aadb64434aecf1e54ae8b0f01d8143fb
    
    Author: Joey <joey@lineageos.org>
    Date:   Fri Feb 7 22:18:51 2020 +0100
    
        DerpLauncher: fix protected apps behaviour
    
        If no secure lockscreen is set we should not show
        the app list
    
        Also make icons' touchable area larger
    
        Change-Id: Id398e98cb4d8fdae169aae194211b98e6441b4cf
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Joey <joey@lineageos.org>
    Date:   Sat Apr 4 10:57:37 2020 +0200
    
        DerpLauncher: only allow hidden apps when no secure keyguard is set
    
        Change-Id: I670a61ff3fee4d7c7d9623ce8810790f1b25224a
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Joey <joey@lineageos.org>
    Date:   Wed Aug 5 13:23:03 2020 +0200
    
        DerpLauncher: improve protected app names style
    
        Change-Id: I09e9995f0eb34de23353166e302481d064fb3058
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: programminghoch10 <hoch10@kabelbw.de>
    Date:   Mon Apr 18 12:39:22 2022 +0200
    
        DerpLauncher: adapt hidden and protected apps for S
    
        * Specifying the AppFilter class in the config.xml does not work anymore
        * Instead we just instantiate our own HiddenAppsFilter class in the LauncherModel
    
        Change-Id: I2a6791083261a580327d99dd1e93eaa019de5bc8
    
    Change-Id: I10c35407820373a1d5f84b237ac449398e076dcd
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>

--- src/com/android/launcher3/settings/SettingsMisc.java
+++ src/com/android/launcher3/settings/SettingsMisc.java
@@ -56,6 +56,7 @@ import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
+import com.android.launcher3.lineage.trust.TrustAppsActivity;
 import com.android.launcher3.model.WidgetsModel;
 import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.uioverrides.flags.DeveloperOptionsUI;
@@ -95,6 +96,7 @@ public class SettingsMisc extends CollapsingToolbarBaseActivity
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
 
+    public static final String KEY_TRUST_APPS = "pref_trust_apps";
     protected static final String DPS_PACKAGE = "com.google.android.as";
 
     @Override
@@ -315,6 +317,16 @@ public class SettingsMisc extends CollapsingToolbarBaseActivity
                 case SUGGESTIONS_KEY:
                     // Show if Device Personalization Services is present.
                     return isDPSEnabled(getContext());
+                case KEY_TRUST_APPS:
+                    preference.setOnPreferenceClickListener(p -> {
+                        Utilities.showLockScreen(getActivity(),
+                                getString(R.string.trust_apps_manager_name), () -> {
+                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
+                            startActivity(intent);
+                        });
+                        return true;
+                    });
+                    return true;
             }
 
             return true;
