63f6846062bd90919eaadcf40dfe65501809b1c1
Trishiraj <gamerprince.exp@gmail.com>
2021-09-23 03:34:14 +0530

    DerpLauncher: Add toggle for translucent hotseat background
    neobuddy89: Disabled by default.
    
    Squashed below commits:
        From: Dil3mm4 <dil3mm4.dev@gmail.com>
        Date: Wed, 30 Dec 2020 20:38:39 +0000
        Subject: Appseat: add a bkg
    
        From: StarkDroid <gamerprince.exp@gmail.com>
        Date: Tue, 31 May 2022 16:16:56 -0500
        Subject: Hotseat background: Corner radius only at the top
    
        Rounding off at all sides makes it look like its not attached to anything
    
        From: minaripenguin <alexfinhart@gmail.com>
        Date: Fri, 22 Jul 2022 05:49:42 +0800
        Subject: Launcher3: Link hotseat bg corner radius to dialogCornerRadius attribute
    
    Change-Id: I7333e3fbe5e73188920f09c81304b3f6e8a5fc3e
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -161,6 +161,7 @@ public final class Utilities {
     public static final String LENS_URI = "google://lens";
 
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
+    public static final String KEY_SHOW_HOTSEAT_BG = "pref_show_hotseat_bg";
 
     /**
      * Returns true if theme is dark.
@@ -861,4 +862,8 @@ public final class Utilities {
         return prefs.getBoolean(KEY_DOCK_SEARCH, true);
     }
 
+    public static boolean isHotseatBgEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_SHOW_HOTSEAT_BG, false);
+    }
 }
