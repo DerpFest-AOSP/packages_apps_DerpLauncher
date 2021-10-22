198f428d720f57aa342f1f7e85eda3842cae40dd
Saul Henriquez <saul_henriquez@hotmail.com>
2021-10-21 20:30:27 -0600

    DerpLauncher: Allow widgets to have 1 row as minimum size
    Squashed with:
    
        Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
        Date:   Wed Jan 15 18:15:53 2025 +0100
    
        fixup! DerpLauncher: Allow widgets to have 1 row as minimum size
    
        If spanY is zero it can cause a crash. Ensure it's always >=1.
    
        Change-Id: Iee9f472055d39e2c5db754a345760154f1f75374
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- src/com/android/launcher3/widget/LauncherAppWidgetProviderInfo.java
+++ src/com/android/launcher3/widget/LauncherAppWidgetProviderInfo.java
@@ -95,7 +95,7 @@ public class LauncherAppWidgetProviderInfo extends AppWidgetProviderInfo
 
     public void initSpans(Context context, InvariantDeviceProfile idp) {
         int minSpanX = 0;
-        int minSpanY = 0;
+        int minSpanY = 1;
         int maxSpanX = idp.numColumns;
         int maxSpanY = idp.numRows;
         int spanX = 0;
@@ -110,7 +110,7 @@ public class LauncherAppWidgetProviderInfo extends AppWidgetProviderInfo
             minSpanX = Math.max(minSpanX,
                     getSpanX(widgetPadding, minResizeWidth, dp.cellLayoutBorderSpacePx.x,
                             cellSize.x));
-            minSpanY = Math.max(minSpanY,
+            minSpanY = Math.min(minSpanY,
                     getSpanY(widgetPadding, minResizeHeight, dp.cellLayoutBorderSpacePx.y,
                             cellSize.y));
 
