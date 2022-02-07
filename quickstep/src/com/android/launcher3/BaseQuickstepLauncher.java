358ab8fce78103cc90830ca826398fd5990fff3c
LibXZR <i@xzr.moe>
2022-02-07 10:59:50 +0800

    DerpLauncher: Introduce memory info in overview
    This is convenient for monitoring device status and many OEMs have their design for this. It's time to have our own one.
    
    [PacMM79: Adapt to crdroid launcher. Update quickstep overview actions dimens.xml]
    
    Change-Id: I890c86c798055b1964506d9a4df1cef386562160
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/BaseQuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/BaseQuickstepLauncher.java
@@ -78,6 +78,7 @@ import com.android.quickstep.util.RemoteAnimationProvider;
 import com.android.quickstep.util.RemoteFadeOutAnimationListener;
 import com.android.quickstep.util.SplitSelectStateController;
 import com.android.quickstep.util.TISBindHelper;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
 import com.android.systemui.shared.system.ActivityManagerWrapper;
@@ -109,6 +110,7 @@ public abstract class BaseQuickstepLauncher extends Launcher
                     Float.intBitsToFloat(arg1), arg2 != 0);
 
     private OverviewActionsView mActionsView;
+    private MemInfoView mMemInfoView;
 
     private TISBindHelper mTISBindHelper;
     private @Nullable TaskbarManager mTaskbarManager;
@@ -186,6 +188,9 @@ public abstract class BaseQuickstepLauncher extends Launcher
         getDragLayer().recreateControllers();
         if (mActionsView != null) {
             mActionsView.updateVerticalMargin(newMode);
+        }
+        if (mMemInfoView != null) {
+            mMemInfoView.updateVerticalMargin(newMode);
         }
     }
 
@@ -300,13 +305,16 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
         SysUINavigationMode.INSTANCE.get(this).updateMode();
         mActionsView = findViewById(R.id.overview_actions_view);
+        mMemInfoView = findViewById(R.id.meminfo);
         RecentsView overviewPanel = (RecentsView) getOverviewPanel();
         SplitSelectStateController controller =
                 new SplitSelectStateController(mHandler, SystemUiProxy.INSTANCE.get(this),
                         getStateManager(), getDepthController());
-        overviewPanel.init(mActionsView, controller);
+        overviewPanel.init(mActionsView, controller, mMemInfoView);
         mActionsView.setDp(getDeviceProfile());
+        mMemInfoView.setDp(getDeviceProfile());
         mActionsView.updateVerticalMargin(SysUINavigationMode.getMode(this));
+        mMemInfoView.updateVerticalMargin(SysUINavigationMode.getMode(this));
 
         mAppTransitionManager = new QuickstepTransitionManager(this);
         mAppTransitionManager.registerRemoteAnimations();
@@ -358,6 +366,10 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
     public <T extends OverviewActionsView> T getActionsView() {
         return (T) mActionsView;
+    }
+
+    public MemInfoView getMemInfoView () {
+        return mMemInfoView;
     }
 
     @Override
