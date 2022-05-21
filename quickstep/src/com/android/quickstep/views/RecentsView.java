fc6bebc0ad8b639bd3b71435d8cb9e63aca33903
NurKeinNeid <mralexman3000@gmail.com>
2022-05-21 23:28:04 +0200

    Revert "DerpLauncher: Don't vibrate while scrolling"
    This reverts commit 45ede855141ce067ed84d5c536ab7e9282773a78.

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1304,7 +1304,14 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         vibrateForScroll();
     }
 
-    private void vibrateForScroll() {}
+    private void vibrateForScroll() {
+        long now = SystemClock.uptimeMillis();
+        if (now - mScrollLastHapticTimestamp > mScrollHapticMinGapMillis) {
+            mScrollLastHapticTimestamp = now;
+            VibratorWrapper.INSTANCE.get(mContext).vibrate(SCROLL_VIBRATION_PRIMITIVE,
+                    SCROLL_VIBRATION_PRIMITIVE_SCALE, SCROLL_VIBRATION_FALLBACK);
+        }
+    }
 
     @Override
     protected void determineScrollingStart(MotionEvent ev, float touchSlopScale) {
