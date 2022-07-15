f950dfe76ec205089181744faf338e0c17570fea
Pranav Vashi <neobuddy89@gmail.com>
2022-07-15 17:47:04 +0530

    DerpLauncher: Add recents and app drawer opacity customization
    Do not allow recents opacity to be customizable. This fixes a bug with
    third party launchers being set as default and recents always being
    entirely transpacent.

--- quickstep/src/com/android/launcher3/uioverrides/states/AllAppsState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/AllAppsState.java
@@ -21,11 +21,14 @@ import static com.android.launcher3.logging.StatsLogManager.LAUNCHER_STATE_ALLAP
 
 import android.content.Context;
 
+import androidx.core.graphics.ColorUtils;
+
 import com.android.internal.jank.Cuj;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherState;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.util.Themes;
 import com.android.launcher3.views.ActivityContext;
 import com.android.quickstep.util.BaseDepthController;
@@ -202,8 +205,8 @@ public class AllAppsState extends LauncherState {
 
     @Override
     public int getWorkspaceScrimColor(Launcher launcher) {
-        return launcher.getDeviceProfile().isTablet
+        return ColorUtils.setAlphaComponent(launcher.getDeviceProfile().isTablet
                 ? launcher.getResources().getColor(R.color.widgets_picker_scrim)
-                : Themes.getAttrColor(launcher, R.attr.allAppsScrimColor);
+                : Themes.getAttrColor(launcher, R.attr.allAppsScrimColor), Utilities.getAllAppsOpacity(launcher) * 255 / 100);
     }
 }
--- src/com/android/launcher3/states/HintState.java
+++ src/com/android/launcher3/states/HintState.java
@@ -25,6 +25,7 @@ import androidx.core.graphics.ColorUtils;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherState;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.util.Themes;
 
 /**
@@ -62,7 +63,7 @@ public class HintState extends LauncherState {
     @Override
     public int getWorkspaceScrimColor(Launcher launcher) {
         return ColorUtils.setAlphaComponent(
-                Themes.getAttrColor(launcher, R.attr.overviewScrimColor), 100);
+                Themes.getAttrColor(launcher, R.attr.overviewScrimColor), Utilities.getRecentsOpacity(launcher) * 255 / 100);
     }
 
     @Override
