8336db89cde6bacc74ff678ee5391c3faa81c030
Pranav Vashi <neobuddy89@gmail.com>
2022-07-15 17:47:04 +0530

    DerpLauncher: Add recents and app drawer opacity customization
    Co-authored-by: nift4 <nift4@protonmail.com>
    Change-Id: Id0c48fa7228cc726cf608b72a206a209a54e85fb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -174,6 +174,9 @@ public final class Utilities {
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
     public static final String KEY_SMARTSPACE = "pref_smartspace";
     public static final String KEY_ICON_PACK = "pref_icon_pack";
+    public static final String KEY_RECENTS_OPACITY = "pref_recents_opacity";
+    public static final String KEY_APP_DRAWER_OPACITY = "pref_app_drawer_opacity";
+
     /**
      * Returns true if theme is dark.
      */
@@ -894,4 +897,13 @@ public final class Utilities {
         return prefs.getBoolean(KEY_SMARTSPACE, true);
     }
 
+    public static int getRecentsOpacity(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_RECENTS_OPACITY, 100);
+    }
+
+    public static int getAllAppsOpacity(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 100);
+    }
 }
