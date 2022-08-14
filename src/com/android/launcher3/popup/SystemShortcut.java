96b081415a2840baf9392f099234654592ebec4e
NurKeinNeid <mralexman3000@gmail.com>
2022-08-14 23:07:37 +0200

    Revert "DerpLauncher: Use standard launcher method for uninstalling"
    This reverts commit e2cca59239c99748f0663ee5d2cc90b71932c851.

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -7,7 +7,6 @@ import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
-import android.net.Uri;
 import android.view.View;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageView;
@@ -30,7 +29,6 @@ import com.android.launcher3.util.PackageUserKey;
 import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.widget.WidgetsBottomSheet;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 /**
@@ -265,15 +263,11 @@ public abstract class SystemShortcut<T extends Context & ActivityContext> extend
 
         @Override
         public void onClick(View view) {
-            try {
-                Intent intent = Intent.parseUri(view.getContext().getString(R.string.delete_package_intent), 0)
-                    .setData(Uri.fromParts("package", mItemInfo.getTargetComponent().getPackageName(),
-                    mItemInfo.getTargetComponent().getClassName())).putExtra(Intent.EXTRA_USER, mItemInfo.user);
-                mTarget.startActivitySafely(view, intent, mItemInfo);
-                AbstractFloatingView.closeAllOpenViews(mTarget);
-            } catch (URISyntaxException e) {
-                // Do nothing.
-            }
+            String packageName = mItemInfo.getTargetComponent().getPackageName();
+            Intent intent = new PackageManagerHelper(
+                    view.getContext()).getUninstallIntent(packageName);
+            mTarget.startActivitySafely(view, intent, mItemInfo);
+            AbstractFloatingView.closeAllOpenViews(mTarget);
         }
     }
 
