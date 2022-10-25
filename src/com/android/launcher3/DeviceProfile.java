b9dd3afbefd51cc1b11d6871a93c202790cf88d1
Pranav Vashi <neobuddy89@gmail.com>
2022-10-25 10:54:28 +0530

    DerpLauncher: Do not add more space on hotseat with taskbar
    * When taskbar is enabled, hotseat is squished even on non-tablet devices
      which should not be the case. This patch fixes it.
    
    mnri: forward port to qpr1
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -1282,7 +1282,8 @@ public class DeviceProfile {
                     hotseatBarSizePx - hotseatBarBottomPadding - hotseatCellHeightPx;
 
             int hotseatWidth = getHotseatRequiredWidth();
-            int leftSpacing = (availableWidthPx - hotseatWidth) / 2;
+            int leftSpacing = isTablet ? (availableWidthPx - hotseatWidth) / 2
+                    : (availableWidthPx - hotseatWidth) / 2;
             int rightSpacing = leftSpacing;
             // Hotseat aligns to the left with nav buttons
             if (hotseatBarEndOffset > 0) {
@@ -1325,7 +1326,7 @@ public class DeviceProfile {
     }
 
     private int getAdditionalQsbSpace() {
-        return isQsbInline ? hotseatQsbWidth + hotseatBorderSpace : 0;
+        return isTablet && isQsbInline ? hotseatQsbWidth + hotseatBorderSpace : 0;
     }
 
     /**
