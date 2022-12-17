neobuddy89/derp_launcher

DerpLauncher: Use a custom icon factory to decide icon wrapping
Change-Id: Ie287c918106e786cf6a2d77ca7188a2aa7556fa2
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
@@ -69,9 +69,9 @@ import com.android.launcher3.Workspace;
 import com.android.launcher3.WorkspaceLayoutManager;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.folder.FolderIcon;
-import com.android.launcher3.icons.BaseIconFactory;
 import com.android.launcher3.icons.BitmapInfo;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.lineage.icon.LineageIconFactory;
 import com.android.launcher3.model.BgDataModel;
 import com.android.launcher3.model.BgDataModel.FixedContainerItems;
 import com.android.launcher3.model.WidgetItem;
@@ -200,8 +200,8 @@ public class LauncherPreviewRenderer extends ContextWrapper
         }
         mDp.updateInsets(mInsets);
 
-        BaseIconFactory iconFactory =
-                new BaseIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
+        LineageIconFactory iconFactory =
+                new LineageIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
         BitmapInfo iconInfo = iconFactory.createBadgedIconBitmap(new AdaptiveIconDrawable(
                         new ColorDrawable(Color.WHITE), new ColorDrawable(Color.WHITE)),
                 Process.myUserHandle(),

src/com/android/launcher3/icons/LauncherIcons.java
@@ -21,12 +21,13 @@ import android.content.Context;
 import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.graphics.LauncherPreviewRenderer;
+import com.android.launcher3.lineage.icon.LineageIconFactory;
 
 /**
  * Wrapper class to provide access to {@link BaseIconFactory} and also to provide pool of this class
  * that are threadsafe.
  */
-public class LauncherIcons extends BaseIconFactory implements AutoCloseable {
+public class LauncherIcons extends LineageIconFactory implements AutoCloseable {
 
     private static final Object sPoolSync = new Object();
     private static LauncherIcons sPool;

DerpFest-AOSP/packages_apps_DerpLauncher

src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
@@ -74,9 +74,9 @@ import com.android.launcher3.Workspace;
 import com.android.launcher3.WorkspaceLayoutManager;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.folder.FolderIcon;
-import com.android.launcher3.icons.BaseIconFactory;
 import com.android.launcher3.icons.BitmapInfo;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.derp.icon.DerpIconFactory;
 import com.android.launcher3.model.BgDataModel;
 import com.android.launcher3.model.BgDataModel.FixedContainerItems;
 import com.android.launcher3.model.WidgetItem;
@@ -217,8 +217,8 @@ public class LauncherPreviewRenderer extends ContextWrapper
                 mDp.isTaskbarPresent ? 0 : currentWindowInsets.getSystemWindowInsetBottom());
         mDp.updateInsets(mInsets);
 
-        BaseIconFactory iconFactory =
-                new BaseIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
+        DerpIconFactory iconFactory =
+                new DerpIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
         BitmapInfo iconInfo = iconFactory.createBadgedIconBitmap(
                 new AdaptiveIconDrawable(
                         new ColorDrawable(Color.WHITE),

src/com/android/launcher3/icons/LauncherIcons.java
@@ -22,12 +22,13 @@ import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.graphics.LauncherPreviewRenderer;
 import com.android.launcher3.util.Themes;
+import com.android.launcher3.derp.icon.DerpIconFactory;
 
 /**
  * Wrapper class to provide access to {@link BaseIconFactory} and also to provide pool of this class
  * that are threadsafe.
  */
-public class LauncherIcons extends BaseIconFactory implements AutoCloseable {
+public class LauncherIcons extends DerpIconFactory implements AutoCloseable {
 
     private static final Object sPoolSync = new Object();
     private static LauncherIcons sPool;