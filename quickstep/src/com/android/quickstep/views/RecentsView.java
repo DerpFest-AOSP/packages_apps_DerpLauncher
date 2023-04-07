fd9873af16671b79000fc97a282a41b68e7204d2
Pranav Vashi <neobuddy89@gmail.com>
2023-04-07 08:46:03 +0530

    DerpLauncher: Add vibrate for all overview actions
    Change-Id: Ib8e1d039ce47c0b99029dbcafcd9def59e07ad59
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    [ghostrider-reborn: simplify code, add to recents end clear all button]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -4269,6 +4269,9 @@ public abstract class RecentsView<CONTAINER_TYPE extends Context & RecentsViewCo
 
     @SuppressWarnings("unused")
     private void dismissAllTasks(View view) {
+        if (view != null) {
+            VibratorWrapper.INSTANCE.get(getContext()).vibrate(VibratorWrapper.EFFECT_CLICK);
+        }
         runDismissAnimation(createAllTasksDismissAnimation(DISMISS_TASK_DURATION));
         mContainer.getStatsLogManager().logger().log(LAUNCHER_TASK_CLEAR_ALL);
     }
