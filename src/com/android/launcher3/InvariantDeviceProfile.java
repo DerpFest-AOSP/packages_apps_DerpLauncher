f180adf6ca77be0f0efc0e1d838978e75c0530b6
Pranav Vashi <neobuddy89@gmail.com>
2023-04-19 08:26:00 -0400

    DerpLauncher: Switch to icon pack theming preview
    Ref:
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/8155a4f3db5888b578aecb8904529a22f962968e
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/174d11c6fa88e85920ec078af506fc56ef3bccd4
    
    YAAP:
    * Tweaked to our usage
    * Improved some horrendous code
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -54,6 +54,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.annotation.XmlRes;
 import androidx.core.content.res.ResourcesCompat;
 
+import com.android.launcher3.customization.IconDatabase;
 import com.android.launcher3.icons.DotRenderer;
 import com.android.launcher3.model.DeviceGridState;
 import com.android.launcher3.provider.RestoreDbTask;
@@ -142,6 +143,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
     public int numFolderColumns;
     public float[] iconSize;
     public float[] iconTextSize;
+    public String iconPack;
     public int iconBitmapSize;
     public int fillResIconDpi;
     public @DeviceType int deviceType;
@@ -338,6 +340,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
             case KEY_ALLAPPS_THEMED_ICONS:
             case KEY_ICON_SIZE:
             case KEY_FONT_SIZE:
+            case IconDatabase.KEY_ICON_PACK:
             case KEY_MAX_LINES:
                 onConfigChanged(mContext);
                 break;
@@ -394,6 +397,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
         for (int i = 1; i < iconSize.length; i++) {
             maxIconSize = Math.max(maxIconSize, iconSize[i]);
         }
+        iconPack = IconDatabase.getGlobal(context);
         iconBitmapSize = ResourceUtils.pxFromDp(maxIconSize, metrics);
         fillResIconDpi = getLauncherIconDensity(iconBitmapSize);
 
@@ -492,7 +496,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
     private Object[] toModelState() {
         return new Object[]{
                 numColumns, numRows, numSearchContainerColumns, numDatabaseHotseatIcons,
-                iconBitmapSize, fillResIconDpi, numDatabaseAllAppsColumns, dbFile};
+                iconPack, iconBitmapSize, fillResIconDpi, numDatabaseAllAppsColumns, dbFile};
     }
 
     private void onConfigChanged(Context context) {
