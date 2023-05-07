5c1c4a97bdf41bb43c9a9750d7fd7ca12b0057c9
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-07 22:32:18 +0700

    DerpLauncher: Use overridden IconProvider instance consistently

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -660,7 +660,7 @@ public final class Utilities {
             if (activityInfo == null) {
                 return null;
             }
-            mainIcon = IconProvider.INSTANCE.get(context).getIcon(
+            mainIcon = appState.getIconProvider().getIcon(
                     activityInfo, appState.getInvariantDeviceProfile().fillResIconDpi);
         } else if (info.itemType == LauncherSettings.Favorites.ITEM_TYPE_DEEP_SHORTCUT) {
             List<ShortcutInfo> siList = ShortcutKey.fromItemInfo(info)
