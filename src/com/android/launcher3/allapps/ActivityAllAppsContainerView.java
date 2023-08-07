a865af300aa6d44ac4680dc4ed0afbd046d21f9b
nift4 <nift4@protonmail.com>
2023-08-07 19:47:20 +0200

    DerpLauncher: respect transparent app drawer in tablet UI too
    excluding taskbar is a design decision as it might look weird/unreadable
    over recents or random app content
    
    Change-Id: I2d0e24b4340e7bea923b15dff73481c4088223c9
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -198,6 +198,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
 
         mScrimColor = ColorUtils.setAlphaComponent(Themes.getAttrColor(context,
                 R.attr.allAppsScrimColor), Utilities.getAllAppsOpacity(context) * 255 / 100);
+        mBottomSheetBackgroundColor = mScrimColor;
         mHeaderThreshold = getResources().getDimensionPixelSize(
                 R.dimen.dynamic_grid_cell_border_spacing);
         mHeaderProtectionColor = Themes.getAttrColor(context, R.attr.allappsHeaderProtectionColor);
@@ -292,8 +293,6 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
                 0,
                 0 // Bottom left
         };
-        mBottomSheetBackgroundColor =
-                Themes.getAttrColor(getContext(), R.attr.materialColorSurfaceDim);
         updateBackgroundVisibility(mActivityContext.getDeviceProfile());
         mSearchUiManager.initializeSearch(this);
     }
