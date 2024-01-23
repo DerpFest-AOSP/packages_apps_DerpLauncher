6a5077414acf28aaa847dc6f0686d4c6983448a6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-23 18:16:30 +0530

    DerpLauncher: Remove excess qsb horizontal margin
    Change-Id: I04eff9c4fb6caa2b7ca4fbccfaf79f2dded2873b

--- src/com/android/launcher3/Hotseat.java
+++ src/com/android/launcher3/Hotseat.java
@@ -154,9 +154,6 @@ public class Hotseat extends CellLayout implements Insettable {
                 ? dp.hotseatQsbWidth
                 : getShortcutsAndWidgets().getMeasuredWidth();
 
-        qsbWidth += (getContext().getResources().getDimensionPixelSize(
-                R.dimen.hotseat_qsb_excess_horizontal_margin)) * 2;
-
         mQsb.measure(MeasureSpec.makeMeasureSpec(qsbWidth, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec(dp.hotseatQsbHeight, MeasureSpec.EXACTLY));
     }
