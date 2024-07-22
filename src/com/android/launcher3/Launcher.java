f2960f462af2f2efe711430cc6a6789f6be5d5b0
Ido Ben-Hur <idoybh2@gmail.com>
2024-07-22 15:41:38 +0300

    DerpLauncher: Always hide the keyboard when the drawer is minimized
    if you manually minimize it with the keyboard open it just stays there

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -151,6 +151,7 @@ import android.view.WindowInsetsAnimation;
 import android.view.WindowManager.LayoutParams;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.animation.OvershootInterpolator;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 import android.window.BackEvent;
 import android.window.OnBackAnimationCallback;
@@ -419,6 +420,8 @@ public class Launcher extends StatefulActivity<LauncherState>
     private final SettingsCache.OnChangeListener mNaturalScrollingChangedListener =
             enabled -> mIsNaturalScrollingEnabled = enabled;
 
+    private InputMethodManager mInputMethodManager;
+
     public static Launcher getLauncher(Context context) {
         return fromContext(context);
     }
@@ -600,6 +603,9 @@ public class Launcher extends StatefulActivity<LauncherState>
                     RuleController.parseRules(this, R.xml.split_configuration));
         }
         TestEventEmitter.INSTANCE.get(this).sendEvent(TestEvent.LAUNCHER_ON_CREATE);
+
+        mInputMethodManager = (InputMethodManager) mWorkspace.getContext().getSystemService(
+                Context.INPUT_METHOD_SERVICE); 
     }
 
     protected ModelCallbacks createModelCallbacks() {
@@ -2891,7 +2897,11 @@ public class Launcher extends StatefulActivity<LauncherState>
      * @param progress Transition progress from 0 to 1; where 0 => home and 1 => all apps.
      */
     public void onAllAppsTransition(float progress) {
-        // No-Op
+        if (progress > 0) return;
+        if (mAppsView == null) return;
+        if (mInputMethodManager == null) return;
+        // make sure the keyboard is hidden when the AllApps view is closed
+        mInputMethodManager.hideSoftInputFromWindow(mAppsView.getWindowToken(), 0);
     }
 
     /**
